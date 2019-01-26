e2583e835fda6a90f702d6278d25c7d0dc1a4916
alessandroscarlatti <violanotesnoextras@gmail.com>
2019-01-26 10:04:43 -0600

    initial commit

---
+++ src/main/java/com/scarlatti/reactspringboot1/ReactSpringBoot1Application.java
@@ -0,0 +1,14 @@
+package com.scarlatti.reactspringboot1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactSpringBoot1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactSpringBoot1Application.class, args);
+	}
+
+}
+
